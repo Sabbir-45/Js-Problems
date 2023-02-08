8d7a0a5ccd8a17242e4cb1e5a7515b30ca0af382
Sabbir-45 <sabbir9011@gmail.com>
2023-02-08 13:10:48 +0600

    3,4,5 Problem Sloved

--- index.js
+++ index.js
@@ -22,3 +22,68 @@ function evenOdd(word) {
     if (word.length % 2 === 0) return "Even";
     else return "Odd";
   }
+
+
+  // Problem 3
+// This function takes in any number, subtracts 7 from it and returns the answer if the difference is less than 7 or else it returns the double of the input number.
+function isLGSeven(number) {
+  const type = typeof number;
+  //This line checks for empty parameter as well.
+  if (type !== "number") return "Please enter a number!";
+
+  let difference = number - 7;
+  if (difference < 7) return difference;
+  else return number * 2;
+}
+
+// Problem 4
+// This function takes in a non empty array and returns the total number of negative numbers(bad data) available;
+function findingBadData(array) {
+  //This line checks for empty parameter as well.
+  if (Array.isArray(array) === false || array.length === 0)
+    return "Please enter an array which cannot be empty";
+
+  let count = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0) count++;
+  }
+
+  return count;
+}
+
+// Problem 5
+function gemsToDiamond(num1, num2, num3) {
+  const totalOfNums = num1 + num2 + num3;
+  const typeNum1 = typeof num1,
+    typeNum2 = typeof num2,
+    typeNum3 = typeof num1;
+  if (
+    typeNum1 !== "number" ||
+    typeNum2 !== "number" ||
+    typeNum3 !== "number" ||
+    num1 < 0 ||
+    num2 < 0 ||
+    num3 < 0 ||
+    totalOfNums % 1 !== 0
+  )
+    return "Please enter 3 valid non empty positive integer value(s)";
+  const gemsConst1 = 21,
+    gemsConst2 = 32,
+    gemsConst3 = 43;
+
+  let diamond1 = gemsConst1 * num1,
+    diamond2 = gemsConst2 * num2,
+    diamond3 = gemsConst3 * num3;
+
+  totalDiamond = diamond1 + diamond2 + diamond3;
+
+  if (totalDiamond > 1000 * 2) return totalDiamond - 2000;
+  else return totalDiamond;
+}
+
+console.log(gemsToDiamond(1, 1, 1));
+console.log(findingBadData([-4, -9, -5, -33, -55]));
+console.log(isLGSeven(-15));
+console.log(evenOdd("Phero"));
+console.log(mindGame(33));
